0c84b5ccb7b16ddfa5d4cee6ca3258b23eaab896
olakayCoder1 <programmerolakay@gmail.com>
2025-05-16 01:32:22 +0100

    `Removed unused import of Separator component from sign-in page`

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -18,7 +18,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Separator } from "@/components/ui/separator";
 import { toast } from "sonner";
 import authService from "@/services/authService";
 
